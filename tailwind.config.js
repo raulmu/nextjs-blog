156c9320d0039470e3e3998a08079c1b177a8eec
raulmu <raulmu@outlook.com>
2020-06-07 15:26:50 -0300

    V0 - Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    purge: [
+        './pages/**/*.js',
+        './components/**/*.js'
+    ],
+    theme: {},
+    variants: {},
+    plugins: [],
+  }
+  
